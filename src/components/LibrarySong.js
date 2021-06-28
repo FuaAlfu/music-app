1f686c306e03649072424efe06d1566e19571490
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-06-28 21:56:06 +0300

    add

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -4,8 +4,10 @@ const LibrarySong = ({song}) => {
     return(
        <div className="library-song">
         <img alt={song.name} src={song.cover}/>
+        <div className="song-description">
        <h3>{song.name}</h3>
        <h3>{song.artist}</h3>
+       </div>
        </div>
         )
 }
